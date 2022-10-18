526d329058d305cd572e1b5d787925c0b75d32b6
JulioManoel <juliobonow@gmail.com>
2022-10-18 14:24:10 -0300

    Unity Folder

---
+++ Unity/Assets/Figma Converter/FigmaConverter.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+
+public class FigmaConverter : EditorWindow {
+
+    string token;
+    string documentID;
+
+    [MenuItem("Tools/Figma Converter")] // Aba onde irá achar a ferramenta
+    public static void ShowWindow() { //Mostrar janela da ferramenta
+        GetWindow<FigmaConverter>("Figma Converter");  //Title da ferramenta
+    }
+
+    private void OnGUI() {
+        GUILayout.Label("Settings", EditorStyles.label);  //Escrita
+        token = EditorGUILayout.TextField("Enter Token:", token);
+        documentID = EditorGUILayout.TextField("Enter URL Document:", documentID);
+        EditorGUILayout.Space();
+
+        if(GUILayout.Button("Apply")) {
+            int cont = 0;
+            for(int i = documentID.Length-1; i > 0; i--){
+                if(documentID[i] == '/'){
+                    cont++;
+                    if(cont == 1)
+                        documentID = documentID.Remove(i);
+                    else if(cont == 2){
+                        documentID = documentID.Remove(0,i);
+                        break;
+                    }
+                }
+            }
+            
+            File api = APIHelper.GetDocument(token, documentID);            
+
+            //Loop Pagina
+            for(int i = 0; i<api.document.children.Length; i++){
+                GameObject empty = new GameObject("Page " + (i+1));
+                
+                // Loop Objeto
+                for(int j = 0; j<api.document.children[i].children.Length; j++){
+                    
+                    var apiObj = api.document.children[i].children[j];
+                    
+                    // Verifica se Objeto é um Retangulo
+                    if(apiObj.type == "RECTANGLE"){
+                        // Altura e Largura
+                        var width = apiObj.absoluteBoundingBox.width/100;
+                        var height = apiObj.absoluteBoundingBox.height/100;
+                        
+                        // Localização nos Eixos X e Y
+                        var x = (apiObj.absoluteBoundingBox.x/100) + (width/2); 
+                        var y = (apiObj.absoluteBoundingBox.y/100) + (height/2);
+                        
+                        // Cores
+                        var r = apiObj.fills[0].color.r;
+                        var g = apiObj.fills[0].color.g;
+                        var b = apiObj.fills[0].color.b;
+                        var a = apiObj.fills[0].color.a;
+                        
+                        // Criação do Objeto
+                        GameObject obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                        obj.transform.position = new Vector3(x,y,(float)(0.01*j));
+                        obj.transform.localScale = new Vector3(width, height, 1);
+                        obj.transform.parent = empty.transform;
+                        
+                        // Variavel teporaria para inserir a cor do Objeto
+                        var tempMaterial = new Material(obj.GetComponent<Renderer>().sharedMaterial);
+                        tempMaterial.color = new Color32((byte)(r * 255), (byte)(g * 255), (byte)(b * 255), (byte)(a * 255));
+                        obj.GetComponent<Renderer>().sharedMaterial = tempMaterial;
+                        Debug.Log("Objeto Criado com Sucesso");
+                    }
+                    //Verifica se é Circulo
+                    else if(api.document.children[0].children[j].type == "ELLIPSE"){
+                        // Altura e Largura
+                        var width = api.document.children[0].children[j].absoluteBoundingBox.width/100;
+                        var height = api.document.children[0].children[j].absoluteBoundingBox.height/100;
+                        // Localização nos Eixos X e Y
+                        var x = (api.document.children[0].children[j].absoluteBoundingBox.x/100) + (width/2); 
+                        var y = (api.document.children[0].children[j].absoluteBoundingBox.y/100) + (height/2);
+                        // Cores
+                        var r = api.document.children[0].children[j].fills[0].color.r;
+                        var g = api.document.children[0].children[j].fills[0].color.g;
+                        var b = api.document.children[0].children[j].fills[0].color.b;
+                        var a = api.document.children[0].children[j].fills[0].color.a;
+                        // Criação do Objeto
+                        GameObject obj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                        obj.transform.position = new Vector3(x,y,(float)(0.01*j));
+                        obj.transform.localScale = new Vector3(width, height, 1);
+                        obj.transform.parent = empty.transform;
+                        // Variavel teporaria para inserir a cor do Objeto
+                        var tempMaterial = new Material(obj.GetComponent<Renderer>().sharedMaterial);
+                        tempMaterial.color = new Color32((byte)(r * 255), (byte)(g * 255), (byte)(b * 255), (byte)(a * 255));
+                        obj.GetComponent<Renderer>().sharedMaterial = tempMaterial;
+                        Debug.Log("Objeto Criado com Sucesso");
+                    }
+                }
+                empty.transform.Rotate(180.0f, 0f, 0f, Space.World);
+            }
+        }
+    }
+}
---
+++ Unity/Assets/Figma Converter/File.cs
@@ -0,0 +1,107 @@
+﻿[System.Serializable]
+public class Constrait{
+    public string type;
+    public string value;
+}
+
+[System.Serializable]
+public class ExportSettings{
+    public string suffix;
+    public string format;
+    public Constrait constrait;
+}
+
+[System.Serializable]
+public class Size{
+    public string width;
+    public string heigth;
+}
+
+[System.Serializable]
+public class PrototypeDevice{
+    public string type;
+    public Size size;
+}
+
+[System.Serializable]
+public class Color{
+    public float r;
+    public float g;
+    public float b;
+    public float a;
+}
+
+[System.Serializable]
+public class Fills{
+    public string blendMode;
+    public string type;
+    public Color color;
+}
+
+[System.Serializable]
+public class Constraints{
+    public string vertical;
+    public string horizontal;
+}
+
+[System.Serializable]
+public class Absolute{
+    public float x;
+    public float y;
+    public float width;
+    public float height;
+}
+
+[System.Serializable]
+public class ChildrenObj{
+    public string id;
+    public string name;
+    public string type;
+    public string blendMode;
+    public Absolute absoluteBoundingBox;
+    public Absolute absoluteRenderBounds;
+    public Constraints constraits;
+    public Fills[] fills;
+    public string[] strokes;
+    public float strokeWeight;
+    public string storekeAlign;
+    public string[] effects;
+}
+
+[System.Serializable]
+public class ChildrenPage{
+    public string id;
+    public string name;
+    public string type;
+    public ChildrenObj[] children;
+    public Color backgroundColor;
+    public string prototypeStartNodeID;
+    public string[] flowStartingPoints;
+    public PrototypeDevice prototypeDevice;
+    public ExportSettings[] exportSettings;
+}
+
+[System.Serializable]
+public class Document{
+    public string id;
+    public string name;
+    public string type;
+    public ChildrenPage[] children;
+}
+
+[System.Serializable]
+public class File
+{
+    public Document document;
+    public string[] components;
+    public string[] componentSets;
+    public string[] schemaVersion;
+    public string[] styles;
+    public string name;
+    public string lastModified;
+    public string thumbnailUrl;
+    public string version;
+    public string role;
+    public string editorType;
+    public string linkAccess;
+}
